4be345daf575d58c645e18898474fb9aef41ce79
Vladislav Stoitsov <dev@vladko.dev>
2025-03-07 13:09:13 +0200

    fix header errror

---
+++ resources/views/components/layouts/app/head.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        
+        <title>{{ $title ?? config('app.name', 'Laravel') }}</title>
+        
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Sofia+Sans:ital,wght@0,1..1000;1,1..1000&display=swap" rel="stylesheet">
+        
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @fluxAppearance
+    </head>
+    <body class="min-h-screen bg-white dark:bg-zinc-800">
+        {{ $slot }}
+        
+        @fluxScripts
+    </body>
+</html>
